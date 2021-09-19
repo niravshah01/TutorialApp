abf038784af17ef0337006a61eeb1cf7f4bf506a
Nirav <shah1852nirav@gmail.com>
2021-09-19 09:52:12 +0530

    App

---
+++ app/src/main/java/com/example/untconvertr/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.untconvertr;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textview;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textview = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hii Click Listener Worked", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int a = Integer.parseInt(s);
+                double p = 2.205 * a;
+                textview.setText("The Corresponding Value in Pound :" + p);
+            }
+        });
+    }
+}
